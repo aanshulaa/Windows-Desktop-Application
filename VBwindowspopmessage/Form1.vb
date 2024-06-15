a3f38d3cd205f0595daa6bad66be60ab8b0f7d48
Anshula Killamsetty <112149471+aanshulaa@users.noreply.github.com>
2024-06-15 15:30:16 +0530

    Add files via upload
    : create a Windows Desktop Application and show a popup with a message in it. We will be using Visual Basic programming language.

---
+++ VBwindowspopmessage/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Show the message box
+        MessageBox.Show("Welcome Apoorve, the co-founder of Slidely AI.")
+
+        ' Close the form (and the application)
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Button clicked!")
+    End Sub
+
+End Class
+
+
+
+
